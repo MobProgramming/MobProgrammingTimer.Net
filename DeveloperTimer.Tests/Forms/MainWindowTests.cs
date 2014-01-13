3fcb7172dce4d50adbfb262120f7169ae89ba778
HunterMob <IS_Development@hunterindustries.com>
2014-01-13 09:14:05 -0800

    Initial commit
    Solution check in

---
+++ DeveloperTimer.Tests/Forms/MainWindowTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ApprovalTests.Wpf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeveloperTimer.Tests.Forms
+{
+    [TestClass]
+    public class MainWindowTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var window = new MainWindow();
+            WpfApprovals.Verify(window);
+        }
+    }
+}
---
+++ DeveloperTimer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+using ApprovalTests.Reporters;
+
+[assembly: AssemblyTitle("DeveloperTimer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeveloperTimer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: UseReporter(typeof(DiffReporter))]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2346f5be-e41b-452e-8f55-fa0a0ec7bb5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
